c4465b594f20833e4c8696d2a59585a15e4a2858
proxy707 <princegaur1998@gmail.com>
2020-06-11 11:44:10 +0530

    files added

--- 9presents.py
+++ 9presents.py
@@ -6,3 +6,9 @@ for i in range(0,len(a)):
     val = i+1
     key[idx] = val
 print(*key)
+
+n=int(input())
+arr=list(map(int,input().split()))
+list1=[arr.index(i)+1 for i in range(1,n+1)]
+for i in list1:
+    print(i,end=" ")
