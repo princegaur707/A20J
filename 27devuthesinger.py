d5c4e9817c95adc4c61aa4e85c98fc3112e27724
proxy707 <princegaur1998@gmail.com>
2020-06-16 14:56:21 +0530

    file changed

--- 27devuthesinger.py
+++ 27devuthesinger.py
@@ -1,8 +1,6 @@
 n,d=map(int,input().split())
 arr=list(map(int,input().split()))
-tot=tottime=0
-for i in range(n):
-    tot+=arr[i]
+tot=sum(arr)
 tottime=tot+(n-1)*10
 if d<tottime:
     print(-1)
