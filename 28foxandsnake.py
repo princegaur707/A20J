b01f0ec08754388d2ef7151c5384329b29ebc098
proxy707 <princegaur1998@gmail.com>
2020-06-16 23:13:32 +0530

    file added

---
+++ 28foxandsnake.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+row1="#"+"."*(m-1)
+row2="."*(m-1)+"#"
+row=row2
+for i in range(1,n+1):
+    if i%2==1:
+        print("#"*m)
+    else:
+        print(row)
+        if row==row1:
+            row=row2
+        else:
+            row=row1
