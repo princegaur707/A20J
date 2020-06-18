eb99024ffaa7cc881c438a04a99d8436bb4ce503
proxy707 <princegaur1998@gmail.com>
2020-06-18 12:04:24 +0530

    file changed

--- 31cakeminator.py
+++ 31cakeminator.py
@@ -10,3 +10,20 @@ for i in range(r):
             col.add(j)
 print(r*c - len(row)*len(col))
 
+r,c=map(int,input().split())
+arr=[];row=[];col=[]
+total=tot=0
+for i in range(r):
+    arr.append(input())
+for i in range(r):
+    for j in range(c):
+        if arr[i][j]=='S':
+            row.append(i)
+            col.append(j)
+for i in range(r):
+    if i not in row:
+        tot+=c
+for j in range(c):
+    if j not in col:
+        total+=r-(tot)//c
+print(total+tot)
