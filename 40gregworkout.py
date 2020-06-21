5c23110ae867710ef992f49dc4fd7bcf881b7f36
proxy707 <princegaur1998@gmail.com>
2020-06-21 11:12:13 +0530

    file added

--- 40gregworkout.py
+++ 40gregworkout.py
@@ -2,13 +2,15 @@ n=int(input())
 mat=list(map(int,input().split()))
 exe=['chest','biceps','back']
 arr=[0,0,0]
-for i in range(n):
+i=0
+while(n>0):
+    n-=3
     try:
-        arr[i]+=mat[i]
-        arr[i+1]+=mat[i+1]
-        arr[i+2]+=mat[i+2]
+        arr[i%3]+=mat[i]
+        arr[(i+1)%3]+=mat[i+1]
+        arr[(i+2)%3]+=mat[i+2]
     except:
-        pass
-print(arr)
+        break
+    i+=3
 ind=arr.index(max(arr))
 print(exe[ind])
