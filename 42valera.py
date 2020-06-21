7abd2e95a9505627b7d9b7d2fb324dd9e32426c1
proxy707 <princegaur1998@gmail.com>
2020-06-22 01:16:11 +0530

    file changed

--- 42valera.py
+++ 42valera.py
@@ -2,24 +2,25 @@ n=int(input())
 mat=[]
 for i in range(n):
     mat.append(list(map(str,input())))
-#print(mat)
-a=mat[0][0]
-b=mat[0][1]
+a=mat[0][0];b=mat[0][1]
 flag=False
+if a==b:
+    flag=True
 for i in range(n):
     for j in range(n):
-        if i==j and mat[i][j]==a:
-            pass
-        else:
-            print("Here")
-            flag=True
-            break
-        if i!=j and mat[i][j]==b:
-            pass
-        else:
-            print("It is")
-            flag=True
-            break
+        if flag==False:
+            if i==j or n-i-1==j:
+                if mat[i][j]==a:
+                    pass
+                else:
+                    flag=True
+                    break
+            else:
+                if mat[i][j]==b:
+                    pass
+                else:
+                    flag=True
+                    break
 if flag:
     print("NO")
 else:
