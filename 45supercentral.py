2dbd204b4922d7a7c33bd49e6428dc2ca8b87b93
proxy707 <princegaur1998@gmail.com>
2020-06-23 23:51:32 +0530

    file changed

--- 45supercentral.py
+++ 45supercentral.py
@@ -7,3 +7,9 @@ sx = sorted(a,key=lambda t:t[0]*10000+t[1])
 sy = sorted(a,key=lambda t:t[1]*10000+t[0])
 ix = set()
 iy = set()
+for i in range(1,n-1):
+    if sx[i][0] == sx[i-1][0] == sx[i+1][0]:
+        ix.add(sx[i])
+    if sy[i][1] == sy[i-1][1] == sy[i+1][1]:
+        iy.add(sy[i])
+print(len(ix&iy))
