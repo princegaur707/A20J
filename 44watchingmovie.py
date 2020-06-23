ea47ca88d7bc2a1ea4b286fa9cc46d6e76540f17
proxy707 <princegaur1998@gmail.com>
2020-06-23 22:09:43 +0530

    file changed

--- 44watchingmovie.py
+++ 44watchingmovie.py
@@ -1,17 +1,3 @@
-n,x=map(int,input().split())
-mat=[]
-for i in range(n):
-    mat.append(list(map(int,input().split())))
-current=0;i=time=0
-while(i<n):
-    if current+x<mat[i][0]:
-        current+=x
-    else:
-        time+=mat[i][1]-current
-        current+=time
-        i+=1
-print(time)
-
 n,k = map(int,input().split())
 cur , res = 0,0
 for i in range(n):
