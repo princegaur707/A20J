162f7c1279fc7e5d3856e2e518cb3ebbda030cf4
proxy707 <princegaur1998@gmail.com>
2020-06-23 22:08:09 +0530

    file changed

--- 44watchingmovie.py
+++ 44watchingmovie.py
@@ -11,3 +11,11 @@ while(i<n):
         current+=time
         i+=1
 print(time)
+
+n,k = map(int,input().split())
+cur , res = 0,0
+for i in range(n):
+    l,r = map(int,input().split())
+    res += (r-l+1) + (l-cur-1)%k
+    cur = r
+print(res)
