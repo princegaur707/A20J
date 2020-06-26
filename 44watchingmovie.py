0b8cb633b976187a1770ba84acb478e131f073f4
proxy707 <princegaur1998@gmail.com>
2020-06-26 22:56:49 +0530

    file added

--- 44watchingmovie.py
+++ 44watchingmovie.py
@@ -3,6 +3,7 @@ cur , res = 0,0
 for i in range(n):
     l,r = map(int,input().split())
     res += (r-l+1) + (l-cur-1)%k
+    print("l,cur:",l,cur)
+    print("r-l+1,l-cur-1,(l-cur-1)%k,res:",r-l+1,l-cur-1,(l-cur-1)%k,res)
     cur = r
-    print("r-l-1,l-cur-1,(l-cur-1)%k,res:",r-l-1,l-cur-1,(l-cur-1)%k,res)
 print(res)
