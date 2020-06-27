6b0bd9fab6ebb29d65561567bdc2117b042ed920
proxy707 <princegaur1998@gmail.com>
2020-06-27 19:41:36 +0530

    file changed

--- 45supercentral.py
+++ 45supercentral.py
@@ -1,15 +1,15 @@
-n =  int(input())
-a = []
+n=int(input())
+a=[]
 for i in range(n):
-    x,y = map(int,input().split())
+    x,y=map(int,input().split())
     a.append((x,y))
-sx = sorted(a,key=lambda t:t[0]*10000+t[1])
-sy = sorted(a,key=lambda t:t[1]*10000+t[0])
-ix = set()
-iy = set()
+sx=sorted(a,key=lambda x:(x[0],x[1]))
+sy=sorted(a,key=lambda x:(x[1],x[0]))
+ix=set()
+iy=set()
 for i in range(1,n-1):
-    if sx[i][0] == sx[i-1][0] == sx[i+1][0]:
+    if sx[i][0]== sx[i-1][0] == sx[i+1][0]:
         ix.add(sx[i])
-    if sy[i][1] == sy[i-1][1] == sy[i+1][1]:
+    if sy[i][1]== sy[i-1][1] == sy[i+1][1]:
         iy.add(sy[i])
 print(len(ix&iy))
