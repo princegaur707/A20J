7c25827d21b7354c9437f51b139b6bfa27f77a61
proxy707 <princegaur1998@gmail.com>
2020-06-27 22:18:32 +0530

    file added

--- 46romaandlucky.py
+++ 46romaandlucky.py
@@ -9,3 +9,15 @@ n,k = map(int,input().split())
 a = list(map(int,input().split()))
 a = list(filter(lambda x:cnt(x,k),a))
 print(len(a))
+
+n,k = map(int,input().split())
+cnt=0
+arr= map(int,input().split())
+for i in arr:
+    count=0
+    i=str(i)
+    count=i.count('4')
+    count+=i.count('7')
+    if count<=k:
+        cnt+=1
+print(cnt)
