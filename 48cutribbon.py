2183d47a89cff421f5df1bdd4c9a1c7f948e34f0
proxy707 <princegaur1998@gmail.com>
2020-06-28 18:46:13 +0530

    file changed

---
+++ 48cutribbon.py
@@ -0,0 +1,19 @@
+import math
+n,a,b,c=map(int,input().split())
+dp = [0]*(n+1)
+for i in range(1,n+1):
+    if i-a<0:
+        x = -math.inf
+    else:
+        x = dp[i-a]
+    if i-b<0:
+        y = -math.inf
+    else:
+        y = dp[i-b]
+    if i-c<0:
+        z = -math.inf
+    else:
+        z = dp[i-c]
+    dp[i] = max(x+1,y+1,z+1)
+print(dp)    
+print(dp[-1])
