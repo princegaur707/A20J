0a450cc873a0c6493e79526afc06836b650a5ccc
proxy707 <princegaur1998@gmail.com>
2020-06-28 08:59:45 +0530

    file changed

--- 46romaandlucky.py
+++ 46romaandlucky.py
@@ -1,14 +1,14 @@
-def cnt(n,k):
-    cnt = 0
+def count(n,k):
+    cnt=0
     while n:
         if n%10 in (4,7):
             cnt+=1
-        n = n//10
-    return cnt <= k
-n,k = map(int,input().split())
-a = list(map(int,input().split()))
-a = list(filter(lambda x:cnt(x,k),a))
-print(len(a))
+        n//=10
+    return cnt<=k
+n,k=map(int,input().split())
+arr=list(map(int,input().split()))
+arr=list(filter(lambda x: count(x,k),arr))
+print(len(arr))
 
 n,k = map(int,input().split())
 cnt=0
