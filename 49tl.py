3ad2ca023065905476e1c5e75376e07a4d09cb5c
proxy707 <princegaur1998@gmail.com>
2020-06-30 21:22:29 +0530

    file changed

---
+++ 49tl.py
@@ -0,0 +1,10 @@
+n,m = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+v = min(a)
+p = max(a)
+c = min(b)
+if max(2*v,p) < c:
+    print(max(2*v,p))
+else:
+    print(-1)
