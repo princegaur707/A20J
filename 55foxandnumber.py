4a1b566f6d079dedf3664a2044e60d2507d9a178
proxy707 <princegaur1998@gmail.com>
2020-07-04 15:02:57 +0530

    file changed

--- 55foxandnumber.py
+++ 55foxandnumber.py
@@ -1,5 +1,18 @@
 n=int(input())
 mat=list(map(int,input().split()))
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+k=gcd(mat[0],mat[1])
+for i in range(2,len(mat)):
+    k=gcd(k,mat[i])
+print(k*n)
+
+
+n=int(input())
+mat=list(map(int,input().split()))
 help=set(mat)
 while len(help)!=1:
     mat.sort(reverse=True)
