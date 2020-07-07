7396cc4dc9e6e07d146389aeeeae233634e0153a
proxy707 <princegaur1998@gmail.com>
2020-07-07 23:02:25 +0530

    file added: gclear clear clear

---
+++ 58dzy.py
@@ -0,0 +1,15 @@
+n=int(input())
+res=[]
+for i in range(n):
+    res.append(list(map(int,input().split())))
+res.sort(key= lambda x:(x[0],x[1]))
+print(res)
+flag=False
+for i in range(1,len(res)):
+    if res[i-1][0]<res[i][0] and res[i-1][1]>res[i][1]:
+        flag=True
+        break
+if flag:
+    print("Happy Alex")
+else:
+    print("Poor Alex")
