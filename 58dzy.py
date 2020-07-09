42f34c6168099bc99860df1dfc208c23acbaaedf
proxy707 <princegaur1998@gmail.com>
2020-07-09 22:59:56 +0530

    file changed

--- 58dzy.py
+++ 58dzy.py
@@ -1,16 +1,8 @@
-n=int(input())
-res=[]
+n,m=map(int,input().split())
+a=['B','W']
 for i in range(n):
-    res.append(list(map(int,input().split())))
-res.sort(key= lambda x:(x[0],x[1]))
-print(res)
-flag=False
-for i in range(1,len(res)):
-    if res[i-1][0]<res[i][0] and res[i-1][1]>res[i][1]:
-        flag=True
-        break
-if flag:
-    print("Happy Alex")
-else:
-    
-    print("Poor Alex")
+	s=list(input())
+	for j in range(m):
+		if s[j]=='.':
+			s[j]=a[(i+j)%2]
+	print(''.join(s))
