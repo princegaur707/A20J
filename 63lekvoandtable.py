6202841e3ff9f37afe7a252904e97a1532f68d4c
proxy707 <princegaur1998@gmail.com>
2020-07-12 23:02:56 +0530

    file added

--- 63lekvoandtable.py
+++ 63lekvoandtable.py
@@ -1,26 +1,2 @@
-#***********************************************#
-I=input
-k=int(I())*2
-s=""
-for _ in '0'*4:s+=I()
-print("YNEOS"[any(s.count(str(i))>k for i in range(10))::2])
-
-#*************************************************#
-
-
-from collections import Counter
-k=int(input())
-string=""
-string+=input();string+=input();string+=input();string+=input()
-string=list(string)
-string=[i for i in string if i!='.']
-string=Counter(string)
-count=string.most_common(1)
-try:
-    cnt=count[0][1]
-    if cnt<=2*k:
-        print("YES")
-    else:
-        print("NO")
-except:
-    print("YES")
+n,k=map(int,input().split())
+for i in range(n):print(i*'0 ',k,(n-1-i)*' 0',sep='')
