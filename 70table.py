f3fe966a5d968352a4136e22e1b94d754a8c7951
proxy707 <princegaur1998@gmail.com>
2020-07-31 23:37:11 +0530

    file changed

---
+++ 70table.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+a = []
+for _ in range(n):
+    a.append([int(i) for i in input().split()])
+ok = 0
+ok |= sum(a[0])
+ok |= sum(a[-1])
+for row in a:
+    ok |= row[0] + row[-1]
+print(2 if ok else 4)
