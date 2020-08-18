db222b4b51d4900f690d34e3392ac627809b2d1f
proxy707 <princegaur199@gmail.com>
2020-08-18 14:23:54 +0530

    file changed

---
+++ 72dimaandline.py
@@ -0,0 +1,12 @@
+n = int(input())
+x = list(map(int,input().split()))
+p = x[0]
+m = []
+for i in range(1, n):
+    for a, b in m[:-1]:
+        if (a < p < b) ^ (a < x[i] < b):
+            print("yes")
+            exit()
+    m.append([min(p,x[i]),max(p,x[i])])
+    p = x[i]
+print("no")
